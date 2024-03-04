328d900d453c6391001f7a00296ef694ba945c8e
nikicha2209 <nikicha08@gmail.com>
2024-03-04 18:46:03 +0200

    Added "Inheritance - Lab"

---
+++ C# OOP/Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new Random();
+            int index = random.Next(Count);
+            string removed = this[index];
+            this.RemoveAt(index);
+            return removed;
+        }
+    }
+}
---
+++ C# OOP/Inheritance - Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => this.Any();
+
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var element in collection)
+            {
+                this.Push(element);
+            }
+        }
+    }
+}
