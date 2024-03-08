41c3ad78dc81283b9b5639242d90935ad9d51e73
nikicha2209 <nikicha08@gmail.com>
2024-03-08 18:13:57 +0200

    Added "Interfaces and Abstraction - Lab"

---
+++ C# OOP/Interfaces and Abstraction - Lab/02. Cars/Seat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Start()
+            => "Engine start";
+
+        public string Stop()
+            => "Breaaak!";
+
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}" + Environment.NewLine + 
+                   Start() + Environment.NewLine + Stop();
+        }
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction - Lab/02. Cars/Tesla.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Start()
+            => "Engine start";
+
+        public string Stop()
+            => "Breaaak!";
+
+        public override string ToString()
+        => $"{Color} Tesla {Model} with {Battery} Batteries" + Environment.NewLine +
+           Start() + Environment.NewLine +
+           Stop();
+    }
+}
