3a8a943674ca8364024bfb0f00d1bf6931b35925
nikicha2209 <nikicha08@gmail.com>
2024-03-27 19:17:03 +0200

    Added "SOLID - Lab"

---
+++ C# OOP/SOLID - Lab/P01.Stream_Progress/File.cs
@@ -0,0 +1,14 @@
+﻿namespace P01.Stream_Progress
+{
+    public class File : StreamableFile
+    {
+        private string name;
+
+        public File(string name, int length, int bytesSent) 
+            : base(length, bytesSent)
+        {
+            this.name = name;
+        }
+
+    }
+}
---
+++ C# OOP/SOLID - Lab/P03.Detail_Printer/DetailsPrinter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace P03.DetailPrinter
+{
+    public class DetailsPrinter
+    {
+        private IList<Employee> employees;
+
+        public DetailsPrinter(IList<Employee> employees)
+        {
+            this.employees = employees;
+        }
+
+        public void PrintDetails()
+        {
+            foreach (Employee employee in this.employees)
+            {
+                Print(employee);
+            }
+        }
+
+        private void Print(Employee employee)
+        {
+            employee.Print();
+        }
+
+    }
+}
